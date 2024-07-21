4b16156f3df0861fa953d11d81de920c9a6e253f
harshitaa18 <harshita1874@gmail.com>
2024-07-22 03:04:46 +0530

    new change

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use('/images', express.static(path.join(__dirname, 'upload/images')));
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success:1,
-        image_url:`http://localhost:${port}/images/${req.file.filename}`
+        image_url:`https://foodio-frontend-zlgk.onrender.com//images/${req.file.filename}`
     })
 })
 
